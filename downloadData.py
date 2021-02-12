e8d4e62e354b71274871f9bbb2217492bc1b1030
caleb <crmartin@wustl.edu>
2021-02-12 15:17:47 -0600

    blah blah

--- downloadData.py
+++ downloadData.py
@@ -15,6 +15,21 @@ def grabTickers(filename):
     f.close()
     return tickersymbs[0].split()
 
+#Parse a text file of exp dates and access dates given a ticker symbol.
+#return expry dates and access dates
+#textfile:
+#ACCESS DATE | NEAR TERM | LONG TERM
+#-----------------
+# 02-03-2021 | 2021-02-12 | 2021-03-12
+#-----------------
+def parse_file(tickersymb):
+    file = open(address_txt, "r")
+    node_dict = {}
+    for line in file:
+        name, ssh = lin.split(' | ')
+        node_dict[name] = ssh.rstrip()
+    return node_dict
+
 tickersymbs = grabTickers('tickersymbs.txt')
 
 #create a new ticker class
